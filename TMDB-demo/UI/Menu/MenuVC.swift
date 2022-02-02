6245f58bd7c94068b22b0f87e65af3e345e5b93f
gladiusKatana <garthsnyder@protonmail.com>
2022-02-01 22:29:56 -0500

    moved MenuViewController methods

--- TMDB-demo/UI/Menu/MenuVC.swift
+++ TMDB-demo/UI/Menu/MenuVC.swift
@@ -11,12 +11,27 @@ class MenuViewController: UIViewController, UITableViewDataSource {
     
     weak var delegate: MenuViewControllerDelegate?
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupMenuUI()
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.text = indexPath.row == 0 ? "Top Rated Movies" : "Now Playing Movies"
+        cell.imageView?.image = UIImage(named: indexPath.row == 0 ? "star-icon" : "movie-reel-icon")
+        
+        return cell
+    }
     
     private func setupMenuUI() {
         tableView.dataSource = self
